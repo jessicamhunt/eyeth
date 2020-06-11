984cf38038357024a15a15f056c84ac673f861ea
jessicamhunt <j.m.hunt1018@gmail.com>
2020-06-11 01:07:41 -0400

    Connect to mongodb.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+connectDB = require("./config/db");
 
 const app = express();
+
+// Connect to Database
+connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
