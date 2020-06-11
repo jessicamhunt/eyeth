bcd1cbee68b6c1353909e3ad011c4c362de2f9cd
jessicamhunt <j.m.hunt1018@gmail.com>
2020-06-11 06:07:48 -0400

    Request and body validation for users.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   GET api/users
+// @route   POST api/users
 // @desc    Test route
 // @access  Public
-router.get("/", (req, res) => res.send("Users route"));
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Users route");
+});
 
 module.exports = router;
