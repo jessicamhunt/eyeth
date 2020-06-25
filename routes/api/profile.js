f8a381d31cefd9266c62533908bc491e5aebd014
jessicamhunt <j.m.hunt1018@gmail.com>
2020-06-25 09:37:39 -0400

    Add get route to request profile by user id.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -122,4 +122,29 @@ router.get("/", async (req, res) => {
   }
 });
 
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID (not profile ID)
+// @access  Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile)
+      return res.status(400).json({ msg: "There is no profile for the user" });
+
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
