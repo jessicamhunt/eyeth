e63388e15e85902a025d7c8f15bf8269284f006d
jessicamhunt <j.m.hunt1018@gmail.com>
2020-07-01 08:41:54 -0400

    Create Alert component. Add actions for setting and removing alerts.

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,10 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
 import { SET_ALERT, REMOVE_ALERT } from "./types";
-import uuid from "uuid";
 
-export const setAlert = (msg, alertType) => (dispatch) => {
-  const id = uuid.v4();
+export const setAlert = (msg, alertType, timeout = 1000) => (dispatch) => {
+  const id = uuidv4();
   dispatch({
     type: SET_ALERT,
-    PAYLOAD: { msg, alertType, id },
+    payload: { msg, alertType, id },
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -14,10 +17,10 @@ const Register = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords do not match");
+      setAlert("Passwords do not match", "danger");
     } else {
       console.log(formData);
     }
@@ -91,4 +94,12 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  state,
+});
+
+export default connect(mapStateToProps, { setAlert })(Register);
