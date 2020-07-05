bbff5c178d9dea8df1e52db772b39386a209a7f0
jessicamhunt <j.m.hunt1018@gmail.com>
2020-07-04 21:59:39 -0400

    Change button styles. Add protected route to dashboard component.

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -4,6 +4,8 @@ import NotFound from "../layout/NotFound";
 import Register from "../auth/Register";
 import Login from "../auth/Login";
 import Alert from "../layout/Alert";
+import Dashboard from "../dashboard/Dashboard";
+import PrivateRoute from "../routing/PrivateRoutes";
 
 const Routes = (props) => {
   return (
@@ -12,6 +14,7 @@ const Routes = (props) => {
       <Switch>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <Route component={NotFound} />
       </Switch>
     </section>
