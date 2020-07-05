cc1bb4d8465454b170e0e5871776160ef16d9143
jessicamhunt <j.m.hunt1018@gmail.com>
2020-07-05 05:22:45 -0400

    Create profile component. Capture value of form inputs on event changes.

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -5,6 +5,7 @@ import Register from "../auth/Register";
 import Login from "../auth/Login";
 import Alert from "../layout/Alert";
 import Dashboard from "../dashboard/Dashboard";
+import CreateProfile from "../profile-forms/CreateProfile";
 import PrivateRoute from "../routing/PrivateRoutes";
 
 const Routes = (props) => {
@@ -15,6 +16,7 @@ const Routes = (props) => {
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <PrivateRoute exact path="/create-profile" component={CreateProfile} />
         <Route component={NotFound} />
       </Switch>
     </section>
