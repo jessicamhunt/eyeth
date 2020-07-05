5d78f0e284dd63a1a3c076a685694affe2234a59
jessicamhunt <j.m.hunt1018@gmail.com>
2020-07-05 03:52:42 -0400

    Add action to clear profile on logout. Edit dashboard to welcome user. Show profile or give user option to create one.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  CLEAR_PROFILE,
 } from "./types";
 import { setAlert } from "./alert";
 import setAuthToken from "../utils/setAuthToken";
@@ -98,5 +99,6 @@ export const login = (email, password) => async (dispatch) => {
 // Logout / Clear Profile
 
 export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
