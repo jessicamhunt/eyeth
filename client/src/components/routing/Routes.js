da017fe025944fe24e24609d3bc09f6bfac8064b
jessicamhunt <j.m.hunt1018@gmail.com>
2020-07-07 23:25:13 -0400

    Create components for adding experience and education to profile. Create routes for new components.

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -7,6 +7,8 @@ import Alert from "../layout/Alert";
 import Dashboard from "../dashboard/Dashboard";
 import CreateProfile from "../profile-forms/CreateProfile";
 import EditProfile from "../profile-forms/EditProfile";
+import AddExperience from "../profile-forms/AddExperience";
+import AddEducation from "../profile-forms/AddEducation";
 import PrivateRoute from "../routing/PrivateRoutes";
 
 const Routes = (props) => {
@@ -19,6 +21,8 @@ const Routes = (props) => {
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <PrivateRoute exact path="/create-profile" component={CreateProfile} />
         <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+        <PrivateRoute exact path="/add-experience" component={AddExperience} />
+        <PrivateRoute exact path="/add-education" component={AddEducation} />
         <Route component={NotFound} />
       </Switch>
     </section>
