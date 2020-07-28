0d8fb5c253b5cb9d1ce737b55ef81d0ca6ded39b
jessicamhunt <j.m.hunt1018@gmail.com>
2020-07-28 02:10:24 -0400

    Trying to fix deployment issues.

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express");
-connectDB = require("./config/db");
+const connectDB = require("./config/db");
 const path = require("path");
 
 const app = express();
 
-// Connect to Database
+// Connect Database
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
 // Define Routes
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/posts", require("./routes/api/posts"));
-app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
