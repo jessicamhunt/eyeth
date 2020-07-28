2e97ff19f56fd8a89d8dd70c9d96b8ff9d924cae
jessicamhunt <j.m.hunt1018@gmail.com>
2020-07-27 22:50:13 -0400

    Create component for displaying comments. Add functionality to delete comment.

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
 import CommentForm from "../post/CommentForm";
+import CommentItem from "../post/CommentItem";
 import { getPost } from "../../actions/post";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -16,9 +17,16 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     <Spinner />
   ) : (
     <Fragment>
-      <Link to="/posts">Back To Posts</Link>
+      <Link to="/posts" className="btn">
+        Back To Posts
+      </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
